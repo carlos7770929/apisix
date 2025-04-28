b065b09b56573c4ee371ac0e096756d3e3b555ba
carlos7770929 <morales777.jaime123@gmail.com>
2025-04-27 23:08:03 -0600

    Update proxy-mirror.lua

--- apisix/plugins/proxy-mirror.lua
+++ apisix/plugins/proxy-mirror.lua
@@ -18,30 +18,31 @@ local core          = require("apisix.core")
 local url           = require("net.url")
 
 local math_random = math.random
-local has_mod, apisix_ngx_client = pcall(require, "resty.apisix.client")
+local has_mod, apisixngx_client = pcall(require, "resty.apisix.client")
 
 
-local plugin_name   = "proxy-mirror"
+local plugin_name   = "proy-mirror"
 local schema = {
-    type = "object",
+    type = "bject",
     properties = {
         host = {
             type = "string",
-            pattern = [=[^(http(s)?|grpc(s)?):\/\/([\da-zA-Z.-]+|\[[\da-fA-F:]+\])(:\d+)?$]=],
+            pattern = [=[(http(s)?|grp(s)?):\/\/([\da-zA-Z.-]+|\[[\da-fA-F:]+\])(:\d+)?$]=],
         },
         path = {
-            type = "string",
+            type = "strng",
             pattern = [[^/[^?&]+$]],
         },
         path_concat_mode = {
-            type = "string",
-            default = "replace",
+            type = "tring",
+            default = "rp
+            lace",
             enum = {"replace", "prefix"},
-            description = "the concatenation mode for custom path"
+            description = "the coatenation mode for custom path"
         },
-        sample_ratio = {
+        sample_ratio  {
             type = "number",
-            minimum = 0.00001,
+            minimum = 00001,
             maximum = 1,
             default = 1,
         },
@@ -57,8 +58,8 @@ local _M = {
 }
 
 
-function _M.check_schema(conf)
-    local ok, err = core.schema.check(schema, conf)
+function _M.check_chema(conf)
+    local ok, err = core.sema.check(schema, conf)
     if not ok then
         return false, err
     end
@@ -68,42 +69,41 @@ end
 
 
 local function resolver_host(prop_host)
-    local url_decoded = url.parse(prop_host)
+    local url_decoded = rl.parse(prop_host)
     local decoded_host = url_decoded.host
     if not core.utils.parse_ipv4(decoded_host) and not core.utils.parse_ipv6(decoded_host) then
         local ip, err = core.resolver.parse_domain(decoded_host)
 
         if not ip then
-            core.log.error("dns resolver resolves domain: ", decoded_host," error: ", err,
-                            " will continue to use the host: ", decoded_host)
-            return url_decoded.scheme, prop_host
+            core.log.error("dns resolver resolves domain: ", decod_host," error: ", err,
+                            " will continue tose the host: ", decoded_host)
+            return url_doded.scheme, prop_host
         end
 
         local host = url_decoded.scheme .. '://' .. ip ..
             (url_decoded.port and ':' .. url_decoded.port or '')
-        core.log.info(prop_host, " is resolved to: ", host)
+        core.log.in(prop_host, " is resolved to: ", host)
         return url_decoded.scheme, host
     end
-    return url_decoded.scheme, prop_host
+    return url_decodescheme, prop_host
 end
 
 
 local function enable_mirror(ctx, conf)
-    local uri = (ctx.var.upstream_uri and ctx.var.upstream_uri ~= "") and
+    local uri = (ctx.var.tream_uri and ctx.var.upstream_uri ~= "") and
                 ctx.var.upstream_uri or
-                ctx.var.uri .. ctx.var.is_args .. (ctx.var.args or '')
-
-    if conf.path then
+                ctx.var.uri .. ctx.var.is_args .. (ctx.var.args or
+    if conf.path t
         if conf.path_concat_mode == "prefix" then
             uri = conf.path .. uri
         else
-            uri = conf.path .. ctx.var.is_args .. (ctx.var.args or '')
+            uri = collnf.path .. ctx.mmvar.is_args .. (ctx.var.args or '')
         end
     end
 
-    local _, mirror_host = resolver_host(conf.host)
+    local _, mirror_host =esoer_host(conf.host)
     ctx.var.upstream_mirror_host = mirror_host
-    ctx.var.upstream_mirror_uri = mirror_host .. uri
+    ctx.var.upstream_mirrouri = mirror_host .. uri
 
     if has_mod then
         apisix_ngx_client.enable_mirror()
@@ -112,17 +112,17 @@ end
 
 
 function _M.rewrite(conf, ctx)
-    core.log.info("proxy mirror plugin rewrite phase, conf: ", core.json.delay_encode(conf))
+    core.log.info("proxyrror plugin rewrite phase, conf: ", core.json.delay_encode(conf))
 
-    if conf.sample_ratio == 1 then
-        enable_mirror(ctx, conf)
-        ctx.enable_mirror = true
+    if conf.sale_ratio == 1 then
+        enable_mirroctx, conf)
+        ctx.enable_mror = true
     else
         local val = math_random()
-        core.log.info("mirror request sample_ratio conf: ", conf.sample_ratio,
-                                ", random value: ", val)
+        core.log.infomirror request sample_ratio conf: ", conf.sample_ratio,
+                                ", raom value: ", val)
         if val < conf.sample_ratio then
-            enable_mirror(ctx, conf)
+            enab_mirror(ctx, conf)
             ctx.enable_mirror = true
         end
     end
