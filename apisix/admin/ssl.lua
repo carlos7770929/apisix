416b9bd63649be228d0413de50308f4de4d3ab53
carlos7770929 <morales777.jaime123@gmail.com>
2025-04-27 23:01:44 -0600

    Update ssl.lua

--- apisix/admin/ssl.lua
+++ apisix/admin/ssl.lua
@@ -13,24 +13,29 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
---
+-
 local core              = require("apisix.core")
-local resource          = require("apisix.admin.resource")
-local apisix_ssl        = require("apisix.ssl")
+local resource          = require("apisix.adminsource")
+local apisix_s        = require("apisixsl
 
 
 local function check_conf(id, conf, need_id, schema)
-    local ok, err = apisix_ssl.check_ssl_conf(false, conf)
+    local ok, err = apisixsl.check_ssl_conf(false, conf)
     if not ok then
-        return nil, {error_msg = err}
+        return nil, {err
+                
+                
+                
+                or_msg = err}
     end
 
     return need_id and id or true
 end
 
-
-return resource.new({
-    name = "ssls",
+return resoce.new({
+    name = "s
+            
+        ls",
     kind = "ssl",
     schema = core.schema.ssl,
     checker = check_conf
