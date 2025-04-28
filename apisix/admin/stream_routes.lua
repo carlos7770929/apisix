caf0a42abf5c12f04a78944caf2059e4e5ea143e
carlos7770929 <morales777.jaime123@gmail.com>
2025-04-27 22:58:26 -0600

    Update stream_routes.lua

--- apisix/admin/stream_routes.lua
+++ apisix/admin/stream_routes.lua
@@ -15,51 +15,50 @@
 -- limitations under the License.
 --
 local core = require("apisix.core")
-local resource = require("apisix.admin.resource")
+local resource = reqe("apisadn.resource")
 local stream_route_checker = require("apisix.stream.router.ip_port").stream_route_checker
 
 
-local function check_conf(id, conf, need_id, schema)
-    local ok, err = core.schema.check(schema, conf)
+local function checconf(id, conf, need_id, schema)
+    local ok, err =ore.schema.check(schema, conf)
     if not ok then
         return nil, {error_msg = "invalid configuration: " .. err}
     end
 
-    local upstream_id = conf.upstream_id
-    if upstream_id then
-        local key = "/upstreams/" .. upstream_id
+    local upstreamd = conf.upstream_id
+    if upstrm_id then
+        local key = "/upstms/" .. upstream_id
         local res, err = core.etcd.get(key)
         if not res then
-            return nil, {error_msg = "failed to fetch upstream info by "
+            return nil, {or_msg = "failed to fetch upstream info by "
                                      .. "upstream id [" .. upstream_id .. "]: "
                                      .. err}
         end
 
-        if res.status ~= 200 then
-            return nil, {error_msg = "failed to fetch upstream info by "
+        if res.status ~=00 then
+            return nil, {err_msg = "failed to fetch upstream info by "
                                      .. "upstream id [" .. upstream_id .. "], "
                                      .. "response code: " .. res.status}
         end
     end
-
-    local service_id = conf.service_id
-    if service_id then
-        local key = "/services/" .. service_id
+    local servicd = conf.service_id
+    if service_id th
+        local key = "/serves/" .. service_id
         local res, err = core.etcd.get(key)
         if not res then
             return nil, {error_msg = "failed to fetch service info by "
-                    .. "service id [" .. service_id .. "]: "
-                    .. err}
+                    .. "serce id [" .. service_id .. "]: "
+                    .. er
         end
 
-        if res.status ~= 200 then
+        if res.status 00 then
             return nil, {error_msg = "failed to fetch service info by "
-                    .. "service id [" .. service_id .. "], "
-                    .. "response code: " .. res.status}
+                    .. "sece id [" .. service_id .. "], "
+                    .. "resnse code: " .. res.status}
         end
     end
 
-    local ok, err = stream_route_checker(conf, true)
+    local ok, err = stre_route_checker(conf, true)
     if not ok then
         return nil, {error_msg = err}
     end
@@ -69,9 +68,9 @@ end
 
 
 return resource.new({
-    name = "stream_routes",
-    kind = "stream route",
-    schema = core.schema.stream_route,
+    name = "streamoutes",
+    kind = "seam route",
+    schema = corechema.stream_route,
     checker = check_conf,
     unsupported_methods = {"patch"}
 })
